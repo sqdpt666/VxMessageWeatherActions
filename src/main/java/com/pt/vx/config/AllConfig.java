9ac4572fe9cada59a8a83bdfa7f3e38f4d93891f
sqdpt666 <56298636+sqdpt666@users.noreply.github.com>
2022-09-16 09:36:12 +0800

    你唯一要修改的文件

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -14,25 +14,26 @@ public class AllConfig {
     private static void init(){
         //如果要多个人的话，就复制这个一遍，然后填写里面的内容。这里默认两个人,大伙应该是两个人吧（笑）
         userList.add(getUser(
-                "你自己扫码后的微信号",
-                "她的名字",
-                new BirthDay(1999,8,11,false),  //最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
-                new BirthDay(1999,2,15,true), //最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
-                LocalDate.of(2020,7,8),
-                "江苏省南京市玄武区",
-                "南京",
-                "微信消息模板ID"));
+                "这个人扫码后的微信号",//扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "这个人的称呼",//咋称呼这个人
+                new BirthDay(1999,8,11,false),  //这个人的生日，最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
+                new BirthDay(1999,2,15,true), //这个人对象的生日，最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
+                LocalDate.of(2020,7,8),//这个人的纪念日起点，比如说在一起的那一天
+                "江苏省南京市玄武区",//这个人的详细地址
+                "南京",//这个人在的城市
+                "微信消息模板ID"));//要给这个人发送的模板ID
 
         userList.add(getUser(
-                "你自己扫码后的微信号",
-                "她的名字",
-                new BirthDay(1999,2,15,true),  //最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
-                new BirthDay(1999,8,11,false), //最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
-                LocalDate.of(2020,7,8),
-                "江苏省南京市玄武区",
-                "南京",
-                "微信消息模板ID"));
+                "这个人扫码后的微信号",
+                "这个人的名字",
+                new BirthDay(1999,2,15,true),  //这个人的生日，最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
+                new BirthDay(1999,8,11,false), //这个人对象的生日，最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
+                LocalDate.of(2020,7,8),//这个人的纪念日
+                "江苏省南京市玄武区",//这个人的详细地址
+                "南京",//这个人在的城市
+                "微信消息模板ID"));//要给这个人发送的模板ID
 
+        
     }
 
     public static final List<User> userList = new ArrayList<>();
