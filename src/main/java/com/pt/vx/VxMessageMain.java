cead5fc71dd06958a284675ad8bb658964cf8c96
sqdpt666 <56298636+sqdpt666@users.noreply.github.com>
2022-09-16 14:00:07 +0800

    Update VxMessageMain.java

--- src/main/java/com/pt/vx/VxMessageMain.java
+++ src/main/java/com/pt/vx/VxMessageMain.java
@@ -6,9 +6,11 @@ import com.pt.vx.domain.User;
 import com.pt.vx.service.MessageService;
 
 import java.util.List;
+import java.util.TimeZone;
 
 public class VxMessageMain {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
         MessageService service=new MessageService();
         List<User> userList =   AllConfig.userList;
         userList.forEach(service::sendMessage);
